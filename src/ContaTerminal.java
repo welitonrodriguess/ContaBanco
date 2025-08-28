adb6a3769aec973b72cede2fba2a7e5f24290793
welitonrodriguess <weliton.rodrigues290@icloud.com>
2025-08-27 23:31:42 -0300

    Projeto Banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {""
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Digite o seu nome: "); 
+        String nome = sc.nextLine();
+
+        System.out.println("Digite a sua agencia: "); 
+        String agencia = sc.nextLine();
+
+        System.out.println("Digite a sua conta: "); 
+        int conta = sc.nextInt();
+
+        System.out.println("Digite o seu saldo: "); 
+        double saldo = sc.nextInt();
+
+        System.out.println("Olá " + nome + 
+            ", obrigado por criar conta em nosso banco. Sua agência " + agencia + ", conta " + conta + 
+            " e saldo " + saldo + " já estão disponíveis para saque.");
+
+         sc.close();
     }
 }
